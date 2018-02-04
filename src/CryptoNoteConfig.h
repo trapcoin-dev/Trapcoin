06d40851574b4326ef3938db2fb138b34076dbda
trapcoin-dev <36114945+trapcoin-dev@users.noreply.github.com>
2018-02-04 14:12:46 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -107,7 +107,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
   "188.166.127.80:17340",
-  "188.166.127.81:17340",
+  "188.166.110.113:17340",
 };
 
 struct CheckpointData {
