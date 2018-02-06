ad1f914853aab268aef0fef37e164973078fba1b
Trapcoin-Dev <36114945+trapcoin-dev@users.noreply.github.com>
2018-02-06 23:00:32 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "trapcoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "010a01ff0001f89bf98647029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121012d3951cd14aa546dd7fd8e8aa596b52138e3ea74aec5f32ee336ad04c3e75010";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
